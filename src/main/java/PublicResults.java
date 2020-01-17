2fbb3fa9231db8cd032e29df5e1032445a2e4803
Jean-François Lamy <jf@jflamy.dev>
2020-01-17 08:22:28 -0500

    License

--- src/main/java/PublicResults.java
+++ src/main/java/PublicResults.java
@@ -1,4 +1,9 @@
-
+/***
+ * Copyright (c) 2009-2019 Jean-François Lamy
+ * 
+ * Licensed under the Non-Profit Open Software License version 3.0  ("Non-Profit OSL" 3.0)  
+ * License text at https://github.com/jflamy/owlcms4/blob/master/LICENSE.txt
+ */
 
 import java.io.IOException;
 import java.text.ParseException;
