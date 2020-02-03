10ad0bb17cf4c05fb7a0717aa97b3173791e906e
Jean-François Lamy <jf@jflamy.dev>
2020-02-03 10:51:48 -0500

    attempt to bypass maven for deployment

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,3 +1,2 @@
 #!/bin/sh -x
-mkdir target
-curl -L "https://dl.bintray.com/owlcms/owlcms-maven/app/owlcms/publicresults/4.4.2/publicresults-4.4.2.jar" -o target/publicresults.jar
+curl -L "https://dl.bintray.com/owlcms/owlcms-maven/app/owlcms/publicresults/4.4.2/publicresults-4.4.2.jar" -o publicresults.jar
