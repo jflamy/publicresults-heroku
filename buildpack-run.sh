929069ec848f9f3cadfcc566a343bc7a0b5a132f
Jean-François Lamy <jf@jflamy.dev>
2020-02-04 07:45:39 -0500

    4.4.3.1 heroku

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -x
-curl -L "https://dl.bintray.com/owlcms/owlcms-maven/app/owlcms/publicresults/4.4.3/publicresults-4.4.3.jar" -o publicresults.jar
+curl -L "https://dl.bintray.com/owlcms/owlcms-maven/app/owlcms/publicresults/4.4.3.1/publicresults-4.4.3.1.jar" -o publicresults.jar
