478ec7542a6dbf19e5492a7f70a0f7391ae41081
Jean-François Lamy <jf@jflamy.dev>
2020-02-07 16:53:06 -0500

    renormalize lines

--- src/main/markdown/update-owlcms4.cmd
+++ src/main/markdown/update-owlcms4.cmd
@@ -2,5 +2,5 @@
 set "appName=%HEROKU_APP%"
 set /p "appName=Enter path or just ENTER for default [%appName%] : "
 call heroku plugins:install plugin:builds
-call heroku builds:create --source-url https://github.com/${env.REPO_OWNER}/${env.H_REPO_NAME}/tree/${project.version}/${project.artifactId}.tar.gz
+call heroku builds:create --source-url https://github.com/${env.REPO_OWNER}/${env.H_REPO_NAME}/releases/download/${project.version}/${project.artifactId}.tar.gz --app %appName%
 pause
