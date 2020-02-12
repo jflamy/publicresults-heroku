13ed37f165b618430a94f5a3ed83dc71b2a59098
Jean-François Lamy <jf@jflamy.dev>
2020-02-11 22:09:54 -0500

    4.5.0-alpha2 heroku

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -x
-curl -L "https://dl.bintray.com/owlcms/owlcms-maven/app/owlcms/owlcms/4.5.0-alpha1/owlcms-4.5.0-alpha1.jar" -o owlcms.jar
+curl -L "https://dl.bintray.com/owlcms/owlcms-maven/app/owlcms/owlcms/4.5.0-alpha2/owlcms-4.5.0-alpha2.jar" -o owlcms.jar
--- update-owlcms4.cmd
+++ update-owlcms4.cmd
@@ -2,5 +2,5 @@
 set "appName=%HEROKU_APP%"
 set /p "appName=Enter path or just ENTER for default [%appName%] : "
 call heroku plugins:install plugin:builds
-call heroku builds:create --source-url https://github.com/jflamy-dev/owlcms4-heroku-prerelease/releases/download/4.5.0-alpha1/publicresults-heroku.tar.gz --app %appName%
+call heroku builds:create --source-url https://github.com/jflamy-dev/owlcms4-heroku-prerelease/releases/download/4.5.0-alpha2/publicresults-heroku.tar.gz --app %appName%
 pause
