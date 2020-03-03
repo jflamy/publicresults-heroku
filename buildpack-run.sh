ed3b9238a8018adeb676c108234be57186f04316
Jean-François Lamy <jf@jflamy.dev>
2020-03-03 12:40:12 -0500

    fix buildpack - wrong bintray location

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -x
-curl -L "https://dl.bintray.com/owlcms/owlcms-maven/app/owlcms/owlcms/4.5.2-rc7/publicresults-4.5.2-rc7.jar" -o publicresults.jar
+curl -L "https://dl.bintray.com/owlcms/owlcms-maven/app/owlcms/publicresults/4.5.2-rc7/publicresults-4.5.2-rc7.jar" -o publicresults.jar
