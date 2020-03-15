ddf4dbc8f8998c5dda4f8ea8a52f2ffbfaf49866
Jean-François Lamy <jf@jflamy.dev>
2020-03-15 15:02:57 -0400

    4.6.1-beta1 heroku

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -x
-curl -L "https://dl.bintray.com/owlcms/owlcms-maven/app/owlcms/publicresults/4.6.0-alpha4/publicresults-4.6.0-alpha4.jar" -o publicresults.jar
+curl -L "https://dl.bintray.com/owlcms/owlcms-maven/app/owlcms/publicresults/4.6.1-beta1/publicresults-4.6.1-beta1.jar" -o publicresults.jar
