758c53cc7c78311bdae41025a35dc33074753bd3
Jean-François Lamy <jf@jflamy.dev>
2020-10-13 19:24:21 -0400

    conflict resolution

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,6 +1,3 @@
 #!/bin/sh -x
-<<<<<<< HEAD
-curl -L "https://dl.bintray.com/owlcms/owlcms-maven/app/owlcms/publicresults/4.10.3-rc01/publicresults-4.10.3-rc01.jar" -o publicresults.jar
-=======
 curl -L "https://dl.bintray.com/owlcms/owlcms-maven/app/owlcms/publicresults/4.10.3/publicresults-4.10.3.jar" -o publicresults.jar
->>>>>>> origin/master
+
