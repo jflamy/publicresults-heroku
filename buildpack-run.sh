44faf70633638273772943773588352f233c4d47
Jean-François Lamy <jf@jflamy.dev>
2021-03-22 19:27:47 -0400

    resync with develop

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,6 +1,2 @@
 #!/bin/sh -x
-<<<<<<< HEAD
-curl -L "http://maven.owlcms.mywire.org:8088/releases/app/owlcms/publicresults/4.15.5/publicresults-4.15.5.jar" -o publicresults.jar
-=======
 curl -L "http://maven.owlcms.mywire.org:8088/releases/app/owlcms/publicresults/4.16.0-rc01/publicresults-4.16.0-rc01.jar" -o publicresults.jar
->>>>>>> origin/develop
