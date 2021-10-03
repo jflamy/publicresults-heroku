a2d5e5db42df79f629f08cfa5c6694116f0a3ed5
Jean-François Lamy <lamyjeanfrancois@outlook.com>
2021-10-03 01:41:35 +0000

    4.23.0-alpha01 heroku [skip ci]

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -x
-curl -L "http://maven.owlcms.mywire.org:8088/releases/app/owlcms/publicresults/4.23.0-alpha00/publicresults-4.23.0-alpha00.jar" -o publicresults.jar
+curl -L "http://maven.owlcms.mywire.org:8088/releases/app/owlcms/publicresults/4.23.0-alpha01/publicresults-4.23.0-alpha01.jar" -o publicresults.jar
