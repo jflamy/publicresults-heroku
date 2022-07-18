7bafdf64c24764d7484f019c5ce9f14824d72190
Jean-François Lamy <jf@jflamy.dev>
2022-07-18 15:24:03 -0400

    4.33.0

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,6 +1,2 @@
 #!/bin/sh -x
-<<<<<<< HEAD
-curl -L "http://maven.owlcms.mywire.org:8088/releases/app/owlcms/publicresults/4.32.4/publicresults-4.32.4.jar" -o publicresults.jar
-=======
 curl -L "http://maven.owlcms.mywire.org:8088/releases/app/owlcms/publicresults/4.33.0-rc02/publicresults-4.33.0-rc02.jar" -o publicresults.jar
->>>>>>> origin/develop
