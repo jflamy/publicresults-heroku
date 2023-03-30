0e2f22b074e54f31ef5d8aaacee9c48628a22f03
Jean-François Lamy <lamyjeanfrancois@outlook.com>
2023-03-30 15:21:10 +0000

    38.3.0-rc02 heroku [skip ci]

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -x
-curl -L "http://maven.lerta.ca:8880/releases/app/owlcms/publicresults/36.0.0-beta02/publicresults-36.0.0-beta02.jar" -o publicresults.jar
+curl -L "http://maven.lerta.ca:8880/releases/app/owlcms/publicresults/38.3.0-rc02/publicresults-38.3.0-rc02.jar" -o publicresults.jar
